676302e97d98647c7ba3cb9fea2d452802393d74
maxedhunter <maxwelljhe@gmail.com>
2024-12-10 16:31:45 -0500

    added path return, for now returns a single solution

--- hmm.py
+++ hmm.py
@@ -3,6 +3,12 @@ import numpy as np
 def find_fixed_vector(matrix, power):
     return np.linalg.matrix_power(matrix, power)[0]
 
+# helper function to append to the correct path, checks length and last state
+def append_state(paths, last_state, new_state, new_probability, length):
+    for i, path in enumerate(paths):
+        if len(path) == length and path[-1][0] == last_state:
+            paths[i].append((new_state, new_probability))
+
 def solve_hmm(T, fixed_vector, bag_a, bag_b, sequence):
     # last respective probability
     a = int()
@@ -24,15 +30,17 @@ def solve_hmm(T, fixed_vector, bag_a, bag_b, sequence):
             # temp variables
             last_a = a
             last_b = b
-            
+          
             # A, B --> A
             a_a = last_a * T[0,0] * (bag_a[color] / sum(bag_a.values()))
             b_a = last_b * T[1,0] * (bag_b[color] / sum(bag_b.values()))
 
             if a_a >= b_a:
-                a = a_a                    
+                a = a_a
+                append_state(paths, 'A', 'A', a, i)
             else:
                 a = b_a
+                append_state(paths, 'B', 'A', a, i)
 
             # A, B --> B
             a_b = last_a * T[0,1] * (bag_a[color] / sum(bag_a.values()))
@@ -40,13 +48,15 @@ def solve_hmm(T, fixed_vector, bag_a, bag_b, sequence):
 
             if a_b >= b_b:
                 b = a_b
+                append_state(paths, 'A', 'B', b, i)
             else:
                 b = b_b
+                append_state(paths, 'B', 'B', b, i)
 
-    print(paths)
-
-
-
+    correct_length_paths = [path for path in paths if len(path) == len(sequence)]
+    max_probability = max(path[-1][1] for path in correct_length_paths)
+    return [path for path in correct_length_paths if path[-1][1] == max_probability]
+    
 if __name__ == "__main__":
     T = np.array([[.8, .2], [.5, .5]])
     fixed_vector = find_fixed_vector(T, 100)
@@ -61,6 +71,28 @@ if __name__ == "__main__":
         'black': 3
     }
 
-    sequence = ['red', 'red']
+    sequence = ['red', 'red', 'black', 'red']
 
-    solve_hmm(T, fixed_vector, bag_a, bag_b, sequence)
+    result = solve_hmm(T, fixed_vector, bag_a, bag_b, sequence)
+    print(f"HMMwithPicturesExample: {result}")
+
+    # LONG EXAMPLE 1
+    result2 = solve_hmm(T, fixed_vector, bag_a, bag_b, ['red', 'red', 'black', 'red', 'red', 'red', 'black', 'red'])
+    print(f"HMMwithPicturesExampleDouble: {result2}")
+
+    T_2 = np.array([[.3, .7], [.6, .4]])
+    fixed_vector_2 = find_fixed_vector(T_2, 100)
+
+    bag_a2 = {
+        'red': 3,
+        'black': 2
+    }
+
+    bag_b2 = {
+        'red': 2,
+        'black': 3
+    }
+
+    sequence2 = ['red', 'black', 'black']
+    result3 = solve_hmm(T_2, fixed_vector_2, bag_a2, bag_b2, sequence2)
+    print(f"TakeHomeP4: {result3}")
